e2c48fd2a6054d22f5673fe7af6d247e9f2f6e78
vdparra3 <vdparra3@miners.utep.edu>
2023-10-25 17:45:29 -0600

    update Oct 25

--- demos/3-led-blink/main.c
+++ demos/3-led-blink/main.c
@@ -5,8 +5,8 @@
 
 int main(void) {
   P1DIR |= LEDS;
-  P1OUT &= ~LED_GREEN;
-  P1OUT |= LED_RED;
+  P1OUT &= ~LED_GREEN; //0000 0000
+  P1OUT |= LED_RED;    //0100 0000
 
   configureClocks();		/* setup master oscillator, CPU & peripheral clocks */
   enableWDTInterrupts();	/* enable periodic interrupt */
@@ -16,14 +16,21 @@ int main(void) {
 
 // global state var to count time
 int secondCount = 0;
-
+int led_on = 0;
 void
 __interrupt_vec(WDT_VECTOR) WDT()	/* 250 interrupts/sec */
 {
   secondCount ++;
   if (secondCount >= 250) { 	/* once each sec... */
-    secondCount = 0;		/* reset count */
-    P1OUT ^= LED_GREEN;		/* toggle green LED */
+    secondCount = 0;            /* reset count */
+    P1OUT ^= LED_GREEN; //
+    P1OUT ^= LED_RED;
+    /*     led_on = !led_on;
+    if(led_on)
+      P1OUT ^= LED_GREEN;		/* toggle green LED */
+    /* else
+    P1OUT &= LED_RED;
+    */
   }
 } 
 
--- demos/4-led-blink-pattern/main.c
+++ demos/4-led-blink-pattern/main.c
@@ -1,3 +1,4 @@
+
 //Alternate LEDs from Off, Green, and Red
 #include <msp430.h>
 #include "libTimer.h"
@@ -15,26 +16,58 @@ int main(void) {
 }
 
 // global state vars that control blinking
-int blinkLimit = 5;  // duty cycle = 1/blinkLimit
-int blinkCount = 0;  // cycles 0...blinkLimit-1
+int blinkLimit  = 5;  // duty cycle = 1/blinkLimit
+int blinkCount  = 0;  // cycles 0...blinkLimit-1
 int secondCount = 0; // state var representing repeating time 0…1s
 
+int greenLimit  = 0;
+int redLimit    = 0;
 void
 __interrupt_vec(WDT_VECTOR) WDT()	/* 250 interrupts/sec */
 {
   // handle blinking 
-  blinkCount ++;
-  if (blinkCount >= blinkLimit) { // on for 1 interrupt period
+  /*blinkCount ++;
+   if (blinkCount >= blinkLimit) { // on for 1 interrupt period
     blinkCount = 0;
     P1OUT |= LED_GREEN;
-  } else		          // off for blinkLimit - 1 interrupt periods
+    P1OUT |= LED_RED;
+  } else{		          // off for blinkLimit - 1 interrupt periods
     P1OUT &= ~LED_GREEN;
+    P1OUT &= ~LED_RED;
+    //when green reaches limit -> STOP
+    // RED starts blinking
+   
+  }*/
+  greenLimit++;
+   if(greenLimit >= blinkLimit){
+     // greenLimit = 0;
+     P1OUT |= LED_GREEN;
+   } else{
+     P1OUT &= ~LED_GREEN;
+   }
+   if(greenLimit == 3){
+     P1OUT &= ~LED_GREEN;
 
+   }
+  
+  // 1 sec red 1 sec green
+  /* if(blinkCount >= blinkLimit){
+    blinkCount = 0;
+    P1OUT |= LED_GREEN;
+    P1OUT |= LED_RED;
+    //enters once a sec has been reached
+    if(blinkCount >= blinkLimit){
+      blinkCount = 0;
+      P1OUT &= ~LED_RED;
+    }
+    }*/
   // measure a second
   secondCount ++;
   if (secondCount >= 250) {  // once each second
     secondCount = 0;
-    blinkLimit ++;	     // reduce duty cycle
+    blinkLimit ++;            // reduce duty cycle
+    //  P1OUT ^= LED_GREEN;
+    // P1OUT ^= LED_RED;
     if (blinkLimit >= 8)     // but don't let duty cycle go below 1/7.
       blinkLimit = 0;
   }
